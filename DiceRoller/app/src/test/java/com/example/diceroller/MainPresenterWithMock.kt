e0e9b66cde67711445ddf2c17111eeb0334078b4
azzumwaqar <azzumwaqar@gmail.com>
2022-03-26 10:35:37 +0000

    Mocking added

---
+++ DiceRoller/app/src/test/java/com/example/diceroller/MainPresenterWithMock.kt
@@ -0,0 +1,43 @@
+package com.example.diceroller
+
+import com.google.common.truth.ExpectFailure.assertThat
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoJUnitRunner
+
+
+@RunWith(MockitoJUnitRunner::class)
+class MainPresenterWithMock {
+
+    private lateinit var dice:Dice
+    private lateinit var mainPresenter: MainPresenter
+
+    @Before
+    fun setUp() {
+        dice = mock(Dice::class.java)
+        mainPresenter = MainPresenter(dice)
+    }
+
+    @Test
+    fun check_drawable_for_dice() {
+        //given
+        `when`(dice.roll()).thenReturn(5)
+
+        //when
+        val drawableId = mainPresenter.getDrawableForDice()
+
+        assertEquals(R.drawable.dice_5,drawableId)
+    }
+
+    @Test
+    fun check_roll_is_called() {
+
+        mainPresenter.getDrawableForDice()
+
+        verify(dice).roll()
+    }
+}
