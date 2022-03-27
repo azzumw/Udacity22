1d18194cc50ae671943ad7377d78f56f25204dbb
azzumwaqar <azzumwaqar@gmail.com>
2022-03-27 18:06:02 +0100

    completed app

--- ColourMyViews/app/src/main/java/com/example/colourmyviews/MainActivity.kt
+++ ColourMyViews/app/src/main/java/com/example/colourmyviews/MainActivity.kt
@@ -23,16 +23,20 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         }
     }
 
+
     fun changeBackground(view: View){
         when(view.id){
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
 
             //boxes using android color resources
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.button_red -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.button_green -> binding.boxFourText.setBackgroundResource(R.color.my_green)
+            R.id.button_yellow -> binding.boxFiveText.setBackgroundResource(R.color.my_yellow)
         }
 
         if(view is TextView){
