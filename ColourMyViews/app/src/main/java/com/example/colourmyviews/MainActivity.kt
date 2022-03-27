73013e39f0d458f6db8ffea19e3d2d5cf5e1fc79
azzumwaqar <azzumwaqar@gmail.com>
2022-03-27 17:15:48 +0100

    completed exercise 28

--- ColourMyViews/app/src/main/java/com/example/colourmyviews/MainActivity.kt
+++ ColourMyViews/app/src/main/java/com/example/colourmyviews/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.colourmyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.example.colourmyviews.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        binding.apply {
+            activity = this@MainActivity
+        }
+    }
+
+    fun changeBackground(view: View){
+        when(view.id){
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            //boxes using android color resources
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+        }
+
+        if(view is TextView){
+            view.setTextColor(Color.WHITE)
+        }
     }
 }
