9454a19fe2fc8c1d4846410cd5e2ce3cfb2de001
azzumwaqar <azzumwaqar@gmail.com>
2022-04-03 12:04:40 +0100

    navcontroller setup

--- ShoeInventory/app/src/main/java/com/example/shoeinventory/MainActivity.kt
+++ ShoeInventory/app/src/main/java/com/example/shoeinventory/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.shoeinventory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.shoeinventory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController:NavController
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
+                as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+
+    }
+
 }
