e2941a0f4b1e733a12cb7223aeb9a61d3223f474
azzumwaqar <azzumwaqar@gmail.com>
2022-04-03 12:25:34 +0100

    LoginFragment.kt added

---
+++ ShoeInventory/app/src/main/java/com/example/shoeinventory/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.example.shoeinventory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.shoeinventory.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+    private var _binding : FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
