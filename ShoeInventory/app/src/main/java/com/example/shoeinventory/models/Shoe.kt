2f25bec6839d84322b4c3a186e18c65f52ed4610
azzumwaqar <azzumwaqar@gmail.com>
2022-04-03 13:08:24 +0100

    Shoe.kt model added

---
+++ ShoeInventory/app/src/main/java/com/example/shoeinventory/models/Shoe.kt
@@ -0,0 +1,9 @@
+package com.example.shoeinventory.models
+
+data class Shoe(
+    var name:String,
+    var size:Double,
+    var company:String,
+    var description:String,
+    val images: List<String> = mutableListOf()) {
+}
