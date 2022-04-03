829963a1284f03cf1b58bffd6532e8416bbb2954
azzumwaqar <azzumwaqar@gmail.com>
2022-04-03 12:57:18 +0100

    click setup login and createnew buttons

--- ShoeInventory/app/src/main/java/com/example/shoeinventory/ui/LoginFragment.kt
+++ ShoeInventory/app/src/main/java/com/example/shoeinventory/ui/LoginFragment.kt
@@ -1,13 +1,14 @@
-package com.example.shoeinventory
+package com.example.shoeinventory.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.shoeinventory.R
 import com.example.shoeinventory.databinding.FragmentLoginBinding
-
 
 class LoginFragment : Fragment() {
     private var _binding : FragmentLoginBinding? = null
@@ -18,9 +19,19 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login,container,false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginFrag = this
+    }
+
+    fun loginAndGotoWelcomeScreen(view:View){
+        //navigate to Welcome Screen
+        Toast.makeText(context,"Login",Toast.LENGTH_LONG).show()
     }
 
     override fun onDestroyView() {
