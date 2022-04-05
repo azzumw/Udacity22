fd1e0c616016fdc7e3418260f9d440e4ae409c38
azzumwaqar <azzumwaqar@gmail.com>
2022-04-05 23:29:04 +0100

    added shoelist in the ShoeViewModel.kt

--- ShoeInventory/app/src/main/java/com/example/shoeinventory/viewModels/ShoeViewModel.kt
+++ ShoeInventory/app/src/main/java/com/example/shoeinventory/viewModels/ShoeViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.shoeinventory.viewModels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.shoeinventory.models.Shoe
 
 class ShoeViewModel:ViewModel() {
+    private var _isLoggedIn : Boolean = false
+    val isLoggedIn get() = _isLoggedIn
+
+    private val _shoesList = MutableLiveData<List<Shoe>>()
+    val shoesList get() = _shoesList
+
+
+    override fun onCleared() {
+        super.onCleared()
+        _isLoggedIn = false
+    }
 
 }
