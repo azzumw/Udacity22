8d8e249b9f07163931f4bd7c8db12cc256f79281
azzumwaqar <azzumwaqar@gmail.com>
2022-04-08 15:56:20 +0100

    ShoeDetailFragment.kt added

--- ShoeInventory/app/src/main/java/com/example/shoeinventory/ui/ShoeListFragment.kt
+++ ShoeInventory/app/src/main/java/com/example/shoeinventory/ui/ShoeListFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.shoeinventory.R
 import com.example.shoeinventory.databinding.FragmentShoeListBinding
 import org.w3c.dom.Text
@@ -22,12 +23,16 @@ class ShoeListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = DataBindingUtil.inflate(inflater,R.layout.fragment_shoe_list,container,false)
+        binding.shoeListFrag = this
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
+    fun navigateToDetailScreen(){
+        findNavController().navigate(R.id.action_shoeListFragment_to_shoeDetailFragment)
     }
 
     override fun onDestroyView() {
--- ShoeInventory/app/src/main/java/com/example/shoeinventory/viewModels/ShoeViewModel.kt
+++ ShoeInventory/app/src/main/java/com/example/shoeinventory/viewModels/ShoeViewModel.kt
@@ -1,20 +1,28 @@
 package com.example.shoeinventory.viewModels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.shoeinventory.models.Shoe
 
 class ShoeViewModel:ViewModel() {
-    private var _isLoggedIn : Boolean = false
-    val isLoggedIn get() = _isLoggedIn
+
+    //move this to new User View model
+    private var _isLoggedIn = MutableLiveData(false)
+    val isLoggedIn :LiveData<Boolean>
+    get() = _isLoggedIn
 
     private val _shoesList = MutableLiveData<List<Shoe>>()
     val shoesList get() = _shoesList
 
+    private val _shoe = MutableLiveData<Shoe>()
+    val shoe:LiveData<Shoe>
+    get() = _shoe
 
     override fun onCleared() {
         super.onCleared()
-        _isLoggedIn = false
+        //careful
+        _isLoggedIn.value = false
     }
 
 }
